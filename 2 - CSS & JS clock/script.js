2a1db30c4b18b2dcd8c6a7e316d659cb25e482af
DaniAkash <s.daniakash@gmail.com>
2017-01-26 01:40:55 +0530

    Fix hour and minute angles

--- 2 - CSS & JS clock/script.js
+++ 2 - CSS & JS clock/script.js
@@ -14,13 +14,13 @@ function setDate() {
   const mins = now.getMinutes();
   if(mins === 0) minsHand.className = "hand-plain minute-hand";
   else minsHand.className = "hand minute-hand";
-  const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
+  const minsDegrees = ((mins / 60) * 360) + ((seconds/600)*60) + 90;
   minsHand.style.transform = `rotate(${minsDegrees}deg)`;
 
   const hour = now.getHours();
   if(hour === 0) hourHand.className = "hand-plain hour-hand";
   else hourHand.className = "hand hour-hand";
-  const hourDegrees = ((hour / 12) * 360) + 90;
+  const hourDegrees = ((hour / 12) * 360) + ((mins / 600) * 360) + 90;
   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 }
 
